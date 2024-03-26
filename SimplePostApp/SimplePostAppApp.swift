8cf3814f61ca1054c3328e1b09bd336d049fbe29
shuro <shuro0530@gmail.com>
2024-03-26 15:27:54 +0900

    Initial Commit

---
+++ SimplePostApp/SimplePostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePostAppApp.swift
+//  SimplePostApp
+//
+//  Created by Shuraw on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplePostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
